e74f0e27cc607437461e3136632748f8c4fd4f67
Jamie Corkhill <jamie.corkhill@live.com>
2020-10-14 23:37:18 -0500

    Add client app

--- API/Startup.cs
+++ API/Startup.cs
@@ -28,9 +28,18 @@ namespace API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
             services.AddDbContext<DataContext>(opt => 
             {
                 opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
+            });
+
+            services.AddCors(opt => 
+            {
+                opt.AddPolicy("CorsPolicy", policy => 
+                {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3006");
+                });
             });
         }
 
@@ -47,6 +56,8 @@ namespace API
             app.UseRouting();
 
             app.UseAuthorization();
+
+            app.UseCors("CorsPolicy");
 
             app.UseEndpoints(endpoints =>
             {
